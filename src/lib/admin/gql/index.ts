ab30f28146fbd302cfecf0a3c11017146bb0de9f
tonymushah <tonyramanantsalama@gmail.com>
2024-05-13 11:51:51 +0300

    Added urqls clients

--- src/lib/admin/gql/index.ts
+++ src/lib/admin/gql/index.ts
@@ -1,2 +1,15 @@
 export * from "./fragment-masking";
 export * from "./gql";
+
+import { Client, cacheExchange, fetchExchange } from "@urql/svelte";
+
+const backendUrl = process.env.BACKEND_URL;
+
+if (backendUrl == undefined) {
+  throw new Error("The backend URL is undefined");
+}
+
+export const adminClient = new Client({
+  url: `${backendUrl}/admin`,
+  exchanges: [cacheExchange, fetchExchange],
+});
