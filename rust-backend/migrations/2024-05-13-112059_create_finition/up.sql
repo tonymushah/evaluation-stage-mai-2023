ed8a37d8b2bab4fbc7cdbe0b52dc405b639921d8
tonymushah <tonyramanantsalama@gmail.com>
2024-05-13 14:27:14 +0300

    Added `create_finition` migration

---
+++ rust-backend/migrations/2024-05-13-112059_create_finition/up.sql
@@ -0,0 +1,7 @@
+-- Your SQL goes here
+create table finition(
+    id_finition UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    designation TEXT NOT NULL,
+    prix decimal NOT NULL,
+    duree decimal NOT NULL
+);
--- rust-backend/src/schema.rs
+++ rust-backend/src/schema.rs
@@ -6,3 +6,25 @@ diesel::table! {
         telephone -> Varchar,
     }
 }
+
+diesel::table! {
+    finition (id_finition) {
+        id_finition -> Uuid,
+        designation -> Text,
+        prix -> Numeric,
+        duree -> Numeric,
+    }
+}
+
+diesel::table! {
+    unite (id_unite) {
+        id_unite -> Uuid,
+        designation -> Text,
+    }
+}
+
+diesel::allow_tables_to_appear_in_same_query!(
+    clients,
+    finition,
+    unite,
+);
