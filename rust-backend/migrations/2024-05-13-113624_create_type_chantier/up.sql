057efdf2ab5138fbc9111511b8b2d3eef40466fa
tonymushah <tonyramanantsalama@gmail.com>
2024-05-14 14:59:52 +0300

    added `type_chantier`

--- rust-backend/migrations/2024-05-13-113624_create_type_chantier/up.sql
+++ rust-backend/migrations/2024-05-13-113624_create_type_chantier/up.sql
@@ -2,5 +2,6 @@
 create table type_chantier(
     id_type_chantier UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     nom TEXT NOT NULL,
-    description TEXT NOT NULL
+    description TEXT NOT NULL,
+    duree DECIMAL NOT NULL
 );
