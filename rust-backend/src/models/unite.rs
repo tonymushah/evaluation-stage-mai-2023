8673545ae4eda795f4526338188ac78a3068429c
tonymushah <tonyramanantsalama@gmail.com>
2024-05-14 11:19:51 +0300

    Added `Unite`

---
+++ rust-backend/src/models/unite.rs
@@ -0,0 +1,28 @@
+use crate::schema::unite;
+use async_graphql::SimpleObject;
+use diesel::prelude::*;
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(
+    Debug,
+    Clone,
+    PartialEq,
+    Eq,
+    PartialOrd,
+    Ord,
+    SimpleObject,
+    Identifiable,
+    Selectable,
+    Insertable,
+    Deserialize,
+    Serialize,
+    Queryable,
+)]
+#[diesel(table_name = unite)]
+#[primary_key(id_unite)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Unite {
+    pub id_unite: Uuid,
+    pub designation: String,
+}
