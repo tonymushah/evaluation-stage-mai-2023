1b34f84939d6d94e56be3b1726881d204c4bce7c
tonymushah <tonyramanantsalama@gmail.com>
2024-05-15 16:28:39 +0300

    Added `setContextClient` on client layout

--- src/routes/client/+layout.svelte
+++ src/routes/client/+layout.svelte
@@ -1,5 +1,19 @@
-<script>
+<script lang="ts">
   import ClientHeader from "$lib/client/componnents/ClientHeader.svelte";
+  import {
+    Client,
+    cacheExchange,
+    fetchExchange,
+    setContextClient,
+  } from "@urql/svelte";
+  import type { LayoutServerData } from "./$types";
+  export let data: LayoutServerData;
+  setContextClient(
+    new Client({
+      url: `${data.backend}/front-office`,
+      exchanges: [fetchExchange, cacheExchange],
+    })
+  );
 </script>
 
 {#if !location.pathname.includes("login")}
